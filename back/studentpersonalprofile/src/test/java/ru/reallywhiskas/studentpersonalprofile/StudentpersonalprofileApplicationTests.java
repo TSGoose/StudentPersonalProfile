39c207c3ffc6659298a48bc80a36c74f6a464cee
TSGoose <mrprokst@gmail.com>
2024-05-13 14:59:19 +0400

    base structure

---
+++ back/studentpersonalprofile/src/test/java/ru/reallywhiskas/studentpersonalprofile/StudentpersonalprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.reallywhiskas.studentpersonalprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentpersonalprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
