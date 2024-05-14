12a02fe19b1b472e78bcab4a4b86e3991fae8d15
HaruHaruMoro <a.medvedev2002@gmail.com>
2024-05-14 10:08:58 +0400

    Registrate Student/Teacher

---
+++ front/src/components/Admin/RegistrateStudent.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import '../../css/AdminCss/RegistrateStudent.css'
+
+export default class RegistrateStudent extends Component {
+    render() {
+        return (
+            <div className='new-user-registration'>
+                <h1>РЕГИСТРАЦИЯ СТУДЕНТА</h1>
+                <div className="reg-form">
+                    <form className='registration-form' action="">
+                        <p><input className='data' id='SurnameStudent' type="text" placeholder='Введите фамилию студента' /></p>
+                        <p><input className='data' id='NameStudent' type="text" placeholder='Введите имя студента' /></p>
+                        <p><input className='data' id='MiddleNameStudent' type="text" placeholder='Введите отчество студента' /></p>
+                        <p><input className='data' id='InstituteStudent' type="text" placeholder='Введите институт студента' /></p>
+                        <p><input className='data' id='SpecializationStudent' type="text" placeholder='Введите специальность студента' /></p>
+                        <p><input className='data' id='GroupStudent' type="text" placeholder='Введите группу студента' /></p>
+
+                        <button className='next-btn' type='submit'>Зарегистрировать</button>
+                    </form>
+
+                
+                </div>
+
+            </div>
+        )
+    }
+}
---
+++ front/src/components/Admin/RegistrateTeacher.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import '../../css/AdminCss/RegistrateTeacher.css'
+
+export default class RegistrateTeacher extends Component {
+    render() {
+        return (
+            <div className='new-user-registration'>
+                <h1>РЕГИСТРАЦИЯ ПРЕПОДАВАТЕЛЯ</h1>
+                <div className="reg-form">
+                    <form className='registration-form' action="">
+                        <p><input className='data' id='SurnameTeacher' type="text" placeholder='Введите фамилию преподавателя' /></p>
+                        <p><input className='data' id='NameTeacher' type="text" placeholder='Введите имя преподавателя' /></p>
+                        <p><input className='data' id='MiddleNameTeacher' type="text" placeholder='Введите отчество преподавателя' /></p>
+                        <p><input className='data' id='InstituteTeacher' type="text" placeholder='Введите институт преподавателя' /></p>
+                        <p><input className='data' id='DepartmentTeacher' type="text" placeholder='Введите кафедру преподавателя' /></p>
+
+                        <button className='next-btn' type='submit'>Зарегистрировать</button>
+                    </form>
+
+                
+                </div>
+
+            </div>
+        )
+    }
+}
